1590291715b7eab3f0020defba9c952c1390d0e7
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-01-11 00:04:50 -0300

    Projeto criado

---
+++ src/main/java/com/cursojava/projetocurso/ProjetocursoApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.projetocurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojava/projetocurso/ProjetocursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.projetocurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
